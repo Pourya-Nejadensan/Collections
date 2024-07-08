b9829d704f6c43ee55cc1a340ba65e4d6aec9155
Pourya <pourya.nejadensan@gmail.com>
2024-07-08 10:47:39 +0200

    Step 1: Create a Java class "Student" that should have the following properties: First name, Last name, Student ID.

---
+++ src/main/java/Student.java
@@ -0,0 +1,29 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int studentId;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+}
